ad0c4b8eb503ce976e47e31506bf321710dd4c28
Anda <Anda@192.168.101.87>
2020-06-30 09:04:09 +0300

    defined map variable

---
+++ src/Earthquakes/EarthquakeMap.java
@@ -0,0 +1,11 @@
+package Earthquakes;
+
+import de.fhpotsdam.unfolding.UnfoldingMap;
+import processing.core.PApplet;
+
+public class EarthquakeMap extends PApplet {
+	
+	private static final long serialVersionUID = 1L;
+	private UnfoldingMap map;
+
+}
