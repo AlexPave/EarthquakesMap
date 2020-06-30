fa55c9a192997a8488d0a75dc1eb4089ecb4c324
Anda <Anda@192.168.101.87>
2020-06-30 09:16:00 +0300

    setted up the map from OpenStreet

--- src/Earthquakes/EarthquakeMap.java
+++ src/Earthquakes/EarthquakeMap.java
@@ -1,6 +1,8 @@
 package Earthquakes;
 
 import de.fhpotsdam.unfolding.UnfoldingMap;
+import de.fhpotsdam.unfolding.providers.OpenStreetMap;
+import de.fhpotsdam.unfolding.utils.MapUtils;
 import processing.core.PApplet;
 
 public class EarthquakeMap extends PApplet {
@@ -10,11 +12,14 @@ public class EarthquakeMap extends PApplet {
 	
 	public void setup() {
 		size(950, 600, OPENGL);
-		
+		map = new UnfoldingMap (this, 250, 50, 650, 500, new OpenStreetMap.OpenStreetMapProvider());
+		map.zoomToLevel(2);
+		MapUtils.createDefaultEventDispatcher(this, map);
 	}
 	
 	public void draw() {
 		background(10);
+		map.draw();
 	}
 
 }
