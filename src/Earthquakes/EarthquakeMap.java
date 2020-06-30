8de6c58737583713536a6ef284f9204a57f4768a
Anda <Anda@192.168.101.87>
2020-06-30 09:32:19 +0300

    added createMarker helper method and created markers list
    createMarker - creates markers based on the magnitude of the earthquake

--- src/Earthquakes/EarthquakeMap.java
+++ src/Earthquakes/EarthquakeMap.java
@@ -1,6 +1,13 @@
 package Earthquakes;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import Parsing.ParseFeed;
 import de.fhpotsdam.unfolding.UnfoldingMap;
+import de.fhpotsdam.unfolding.data.PointFeature;
+import de.fhpotsdam.unfolding.marker.Marker;
+import de.fhpotsdam.unfolding.marker.SimplePointMarker;
 import de.fhpotsdam.unfolding.providers.OpenStreetMap;
 import de.fhpotsdam.unfolding.utils.MapUtils;
 import processing.core.PApplet;
@@ -9,18 +16,49 @@ public class EarthquakeMap extends PApplet {
 	
 	private static final long serialVersionUID = 1L;
 	private UnfoldingMap map;
+	private String earthquakesURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.atom";
 	
 	public void setup() {
 		size(950, 600, OPENGL);
 		map = new UnfoldingMap (this, 200, 50, 700, 500, new OpenStreetMap.OpenStreetMapProvider());
 		map.zoomToLevel(2);
 		MapUtils.createDefaultEventDispatcher(this, map);
+		
+		List<Marker> markers = new ArrayList<Marker>();
+	    List<PointFeature> earthquakes = ParseFeed.parseEarthquake(this, earthquakesURL);
+	    
+	    for(PointFeature feature: earthquakes) {
+	    	markers.add(createMarker(feature));
+	    }
 	}
 	
 	public void draw() {
 		background(10);
 		map.draw();
 		addKey();
+	}
+	
+	private SimplePointMarker createMarker(PointFeature feature)
+	{
+		SimplePointMarker mark = new SimplePointMarker(feature.getLocation(), feature.getProperties());
+		Object magObj = feature.getProperty("magnitude");
+	   	float mag = Float.parseFloat(magObj.toString());
+		if(mag<4.0) {
+			mark.setRadius(6);
+			mark.setColor(color(20, 179, 219));
+		}
+		else
+		{
+			if (mag<5.0) {
+				mark.setRadius(10);
+				mark.setColor(color(219, 219, 20));
+			}
+			else {
+				mark.setRadius(14);
+				mark.setColor(color(209, 47, 25));
+			}
+		}
+		return mark;
 	}
 	
 	private void addKey() {
