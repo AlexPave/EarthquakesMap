ebe253f97b9df599e9ba0620eb5fef834154f67b
Anda <Anda@192.168.101.87>
2020-07-01 14:56:10 +0300

    implemented LandQuakeMarker and OceanQuakeMarker classes

--- src/Earthquakes/LandQuakeMarker.java
+++ src/Earthquakes/LandQuakeMarker.java
@@ -4,16 +4,38 @@ import de.fhpotsdam.unfolding.data.PointFeature;
 import processing.core.PGraphics;
 
 public class LandQuakeMarker extends EarthquakeMarker {
-
-	public LandQuakeMarker(PointFeature feature) {
-		super(feature);
-		// TODO Auto-generated constructor stub
+	
+	
+	public LandQuakeMarker(PointFeature quake) {
+		
+		// calling EarthquakeMarker constructor
+		super(quake);
+		
+		// setting field in earthquake marker
+		isOnLand = true;
 	}
 
+	// Draws a centered circle for land quakes
+	
 	@Override
 	public void drawEarthquake(PGraphics pg, float x, float y) {
-		// TODO Auto-generated method stub
 		
+		pg.pushStyle();
+		float radius = this.getRadius();
+		pg.ellipse(x,y,radius, radius);
+		pg.popStyle();
 	}
+	
+
+	// Get the country the earthquake is in
+	public String getCountry() {
+		return (String) getProperty("country");
+	}
+
+
+	
+
+
+
 		
 }
--- src/Earthquakes/OceanQuakeMarker.java
+++ src/Earthquakes/OceanQuakeMarker.java
@@ -1,5 +1,30 @@
 package Earthquakes;
 
-public class OceanQuakeMarker {
+import de.fhpotsdam.unfolding.data.PointFeature;
+import processing.core.PGraphics;
+
+public class OceanQuakeMarker extends EarthquakeMarker {
+	
+	public OceanQuakeMarker(PointFeature quake) {
+		super(quake);
+		
+		isOnLand = false;
+	}
+
+	// Drawing a centered square for Ocean earthquakes
+
+	@Override
+	public void drawEarthquake(PGraphics pg, float x, float y) {
+		pg.pushStyle();
+		float radius = this.getRadius();
+		pg.rect(x,y,radius, radius);
+		pg.popStyle();
+		
+	}
+	
+
+
+	
 
 }
+
